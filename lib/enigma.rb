218c3dd3af3de1af671d282cc9f10ed285de3bbb
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-18 09:48:36 -0600

    Passes existance and has attributes test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,8 @@
-class Enigma 
-
+class Enigma
+  attr_reader :date, :offset, :message
+  def initialize(date, offset, message)
+    @date = date
+    @offset = offset
+    @message = message
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,3 +1,13 @@
-class EnigmaTest
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
 
-end 
+class EnigmaTest < Minitest::Test
+  def test_it_has_attributes
+    enigma = Enigma.new(170920, 57132, "Sage")
+    assert_instance_of Enigma, enigma
+    assert_equal 170920, enigma.date
+    assert_equal 57132, enigma.offset
+    assert_equal "Sage", enigma.message
+  end
+end
