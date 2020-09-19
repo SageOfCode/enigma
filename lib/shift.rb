c5911ef18b6da1dfe1993ffd6553e64f5e46289e
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-18 21:18:35 -0600

    Passes it knows the date code test

--- lib/shift.rb
+++ lib/shift.rb
@@ -9,17 +9,22 @@ class Shift
     key_splits = [split_into_keys[0..1], split_into_keys[1..2], split_into_keys[2..3], split_into_keys[3..4]]
   end
 
+  def assign_keys_to_keys
+    number_values = create_keys.map do |key|
+      key.to_i
+    end
+    letter_keys = ("A".."D").to_a
+    Hash[(letter_keys).zip(number_values)]
+  end
+
   def date_of_today
-    Date.today.strftime("%d%m%y").to_i
+    Date.today.strftime("%d%m%y")
   end
 
   def date_code
-    date_of_today ** 2
+    date_squared = date_of_today ** 2
+    date_squared.to_s[-4..-1].split
   end
 
 
-
-  def method_name
-
-  end
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -6,14 +6,29 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
-  def test_knows_the_date
-    shift = Shift.new
-    assert_equal (180920), shift.date_of_today
-  end
-
   def test_it_knows_keys
     shift = Shift.new
     shift.stubs(:generate_number).returns("2740")
     assert_equal ["02", "27", "74", "40"], shift.create_keys
   end
+
+  def test_it_can_create_keys
+    shift = Shift.new
+    shift.stubs(:create_keys).returns(["02", "27", "74", "40"])
+    expected = {"A" => 2, "B" => 27, "C" => 74, "D" => 40}
+    assert_equal expected, shift.assign_keys_to_keys
+  end
+
+  def test_knows_the_date
+    shift = Shift.new
+    assert_equal 6, shift.date_of_today.length
+  end
+
+  def test_it_knows_the_date_code
+    shift = Shift.new
+    shift.stubs(:date_of_today).returns(190920)
+    expected = ["6400"]
+    assert_equal expected, shift.date_code
+  end
+
 end
