44e4c8bc798e4f02c5206bdd84cd3e8b268f2ce3
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-19 15:42:33 -0600

    Adds find shift method to enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,6 +2,13 @@ require_relative '../lib/shift'
 require_relative '../lib/encrypt'
 
 class Enigma
+  attr_reader :shift
+  def initialize
+    @shift = Shift.new
+  end
 
-  
+  def find_shift
+    # require "pry"; binding.pry
+    @shift.find_shift
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,4 +6,11 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
+  def test_it_can_find_the_shift
+    enigma = Enigma.new
+    enigma.shift.stubs(:create_keys).returns({"A" => 2, "B" => 27, "C" => 74, "D" => 40})
+    enigma.shift.stubs(:create_offset).returns({"A" => 6, "B" => 4, "C" => 0, "D" => 0})
+    expected = {"A" => 8, "B" => 31, "C" => 74, "D" => 40}
+    assert_equal expected, enigma.find_shift
+  end
 end
