99f214cb038b9c3f3b51641a40a190442159a248
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-20 14:32:07 -0600

    Passes the it can rotate letters test

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,10 +6,28 @@ class Encrypt
 
   def break_up(message)
     split_words = []
-    message.chars.each_slice(4) do |array_of_four|
+    message.chars.each_slice(4).with_index do |array_of_four|
       split_words << array_of_four
     end
     split_words
   end
 
+  def rotate_letters(message, shift)
+    array_of_shifted = []
+    message.map do |snippet|
+      a_shift = alphabet.find_index(snippet[0])
+        start_a_shift = alphabet.rotate(a_shift)
+        array_of_shifted << start_a_shift.rotate(shift["A"]).first
+      b_shift = alphabet.find_index(snippet[1])
+        start_b_shift = alphabet.rotate(b_shift)
+        array_of_shifted << start_b_shift.rotate(shift["B"]).first
+      c_shift = alphabet.find_index(snippet[2])
+        start_c_shift = alphabet.rotate(c_shift)
+        array_of_shifted << start_c_shift.rotate(shift["C"]).first
+      d_shift = alphabet.find_index(snippet[3])
+        start_d_shift = alphabet.rotate(d_shift)
+        array_of_shifted << start_d_shift.rotate(shift["D"]).first
+    end
+    array_of_shifted.join
+  end
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -13,10 +13,18 @@ class EncryptTest < Minitest::Test
     assert_equal expected, encrypt.alphabet
   end
 
-  def test_it_can_rotate_letters
+  def test_it_break_up_messages
     encrypt = Encrypt.new
     message = "Sage Mango"
     expected = [["S", "a", "g", "e"], [" ", "M", "a", "n"], ["g", "o"]]
     assert_equal expected, encrypt.break_up(message)
+  end
+
+  def test_it_can_rotate_letters
+    encrypt = Encrypt.new
+    shift = {"A" => 6, "B" => 70, "C" => 6, "D" => 5}
+    expected = "yqmjfbgs"
+    message = [["s", "a", "g", "e"], [" ", "m", "a", "n"]]
+    assert_equal expected, encrypt.rotate_letters(message, shift)
   end
 end
